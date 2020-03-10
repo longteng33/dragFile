f6640f526c470d4c3c5a14092977666f2efcb26d
longteng <475493503@qq.com>
2020-03-10 20:08:32 +0800

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # dragFile
-拖拽文件上传的封装
+## 拖拽文件上传的封装  
+https://longteng33.github.io/dragFile  
+1、被拖放元素的三个事件  
+1）dragstart 开始被拖拽时触发  
+2）drag 在拖拽过程中触发  
+3）dragend拖拽完成时触发  
+
+2、目标元素的四个事件
+1）dragenter进入目标元素时触发
+2）**dragover被拖放元素在目标元素上时触发**  
+3）dragleave被拖放元素从目标元素离开时触发    
+4）**dragdrop被拖放元素在目标元素上，同时鼠标放开时触发**  
+
+3、在dragover中，需要e.preventDefault()，取消默认事件，防止drop不被触发  
+4、在dragdro,拖动对文件的的默认处理方式是在新窗口打开，所以需要取消其默认事件e.preventDefault();  
+
+5、var reader=new FileReader() 创建FileReader对象  
+FileReader对象负责把文件读入内存，并且读取文件中的数据，FileReader对象有一些方法和事件  
+6、FileReader对象的一些方法  
+1）readAsText，将文件中的数据读取为文本数据  
+2）readAsBinaryString，将文件中的数据读取为二进制字符串  
+3）readAsDataURL，将文件中的数据读取为DataURL字符串  
+4)abort终端读取操作  
+7、FileReader对象的一些事件  
+1）onprogress数据读取中触发
+2）onload读取完成时触发（成功才触发）  
+3）onloaded读取完成时触发（无论成功还是失败）  
+
+8、reader.onprogress = function (e) e.loaded为这一段上传中已经上传完成的部分，是数字
+9、封装逻辑：  
+1）传入生成拖拽组件的容器dom  
+2）动态生成拖拽的目标元素、进度条  
+3）在目标元素上关联dragover事件 drop事件 ，取消默认事件  
+4）在reader对象上关联onprogress事件，onload事件  
+5）drop中开始读取，一次读取没完继续递归，直到读取完成
---
+++ dragFile.js
@@ -0,0 +1,122 @@
+function DragFile(dragFileObj) {
+    this.ele = dragFileObj.ele;
+    this.step = dragFileObj.step;
+    this.cb = dragFileObj.cb;
+
+    this.reader = new FileReader();// 创建FileReader对象
+    this.fileCon = null;
+    this.progressCon = null;
+    this.val = null;
+    this.text = null;
+
+    this.file = null;//存储拖拽进来的文件
+    // 用来存储已经上传了多少
+    this.loaded = 0;
+    // file长度
+    this.total = 0;
+
+    this.init();
+    // bindEvent在init之后
+    this.bindEvent();
+}
+
+DragFile.prototype = {
+    init: function () {
+        this.fileCon = document.createElement("div");
+        this.fileCon.classList.add("file-con");
+        this.fileCon.innerText = "拖拽文件到此区域...";
+        this.ele.appendChild(this.fileCon);
+
+        this.progressCon = document.createElement("div");
+        this.progressCon.classList.add("progress-con");
+        this.progressCon.innerHTML =
+            `<div class="progress">
+        <div class="val"></div>
+    </div>
+    <span class="text">0%</span>`;
+        this.ele.appendChild(this.progressCon);
+
+        this.val = document.getElementsByClassName("val")[0];
+        this.text = document.getElementsByClassName("text")[0];
+
+    },
+
+    bindEvent: function () {
+        var _this = this;
+        this.fileCon.ondragover = function (e) {
+            // 取消默认事件
+            e.preventDefault();
+        };
+
+        this.fileCon.ondrop = function (e) {
+            // 默认会把拖到里面的文件打开，取消默认事件
+            e.preventDefault();
+            // 获取拖拽进来的文件
+            _this.file = e.dataTransfer.files[0];
+            _this.total = _this.file.size;
+            _this.readBlob(_this.reader, 0, _this.step);
+
+        }
+
+        // reader对象在读取中的事件
+        this.reader.onprogress = function (e) {
+            // e.loaded为这一段上传中已经上传完成的部分
+            _this.loaded += e.loaded;
+            var per = _this.loaded / _this.total;
+            if (per >= 1) {
+                per = 1;
+            }
+            _this.val.style.width = per * 250 + "px";
+            _this.text.innerHTML = Math.round(per * 100) + "%";
+        }
+
+        // reader对象的一段读取完成时的事件
+        this.reader.onload = function () {
+            if (_this.loaded < _this.total) {
+                _this.readBlob(_this.reader, _this.loaded, _this.step);
+            } else {
+                _this.cb();
+            }
+
+        }
+
+    },
+
+    // readBlob函数用来读取文件
+    readBlob: function (reader, start, step) {
+        if (this.file.slice) {
+            // 如果文件能够被裁切，就将文件裁切为（a至a+b）这一小段，并存储在blob中
+            var blob = this.file.slice(start, start + step);
+        } else {
+            // 如果文件不能被裁切，那么就将文件作为一整段
+            var blob = this.file;
+        }
+        // 在FileReader对象中将裁切好的这一段文件blob以文本数据形式读取进来    
+        reader.readAsText(blob);
+    },
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
